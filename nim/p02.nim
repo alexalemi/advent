20601458575b62d274535461a289d3fed68cbc2c
Alex Alemi <alex.alemi@gmail.com>
2019-01-01 22:37:59 +0000

    with day 2 in nim complete

--- nim/p02.nim
+++ nim/p02.nim
@@ -2,7 +2,7 @@ import seqUtils, tables
 
 let data = toSeq(open("../input/02.txt").lines)
 
-func checksum(data: seq[string]): int =
+proc checksum(data: seq[string]): int =
   var twos, threes = 0
   for line in data:
     let countCounts = toSeq(line.toCountTable.values).toCountTable
@@ -12,4 +12,52 @@ func checksum(data: seq[string]): int =
       threes += 1
   return twos * threes
 
+
+const testdata1 = @[
+  "abcdef",
+  "bababc",
+  "abbcde",
+  "abcccd",
+  "aabcdd",
+  "abcdee",
+  "ababab"]
+
+assert checksum(testdata1) == 12
+
 echo "Answer1: ", data.checksum
+
+proc oneoff(s1: string, s2: string): bool =
+  assert s1.len == s2.len
+  result = false
+  for i in 0..<s1.len:
+    if s1[i] != s2[i]:
+      if result:
+        return false
+      else:
+        result = true
+
+proc common(s1: string, s2: string): string =
+  assert s1.len == s2.len
+  for i in 0..<s1.len:
+    if s1[i] == s2[i]:
+      result.add(s1[i])
+
+
+proc part2(data: seq[string]): string =
+  for i in 0..<data.len:
+    for j in i+1..<data.len:
+      if oneoff(data[i], data[j]):
+        return common(data[i], data[j])
+
+let testdata = @[
+  "abcde",
+  "fghij",
+  "klmno",
+  "pqrst",
+  "fguij",
+  "axcye",
+  "wvxyz"]
+
+assert part2(testdata) == "fgij"
+
+echo "Answer2: ", part2(data)
