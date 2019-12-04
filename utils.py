41ade2a4587d10e89bede41c73842363ff1a5cde
Alex Alemi <alexalemi@gmail.com>
2019-12-03 23:03:49 -0800

    with leaderboard script

--- utils.py
+++ utils.py
@@ -5,15 +5,19 @@ import datetime
 import functools
 import logging
 import os
+import json
 from pathlib import Path
 import pytz
 import sys
 from itertools import tee
+import itertools
+from collections import namedtuple
 
 logging.basicConfig(level=logging.INFO)
 
 EAST_COAST = pytz.timezone("America/New_York")
-TODAY = datetime.datetime.today().astimezone(EAST_COAST)
+TODAY = datetime.datetime.now().astimezone(EAST_COAST)
+YEARS = [2015, 2016, 2017, 2018, 2019]
 
 def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
@@ -23,7 +27,6 @@ def pairwise(iterable):
 
 with open(Path(__file__).resolve().parent / "token.txt") as f:
   TOKEN = f.read().strip()
-
 
 def get_data(day=None, year=None):
   p = Path(__file__).resolve().parent
@@ -85,21 +88,82 @@ data17 = functools.partial(get_data, year=2017)
 data16 = functools.partial(get_data, year=2016)
 data15 = functools.partial(get_data, year=2015)
 
+LEADERBOARD = Path(__file__).resolve().parent / "leaderboard.txt"
+
+def get_leaderboard(force=False):
+  modtime = datetime.datetime.fromtimestamp(LEADERBOARD.stat().st_mtime).astimezone(EAST_COAST)
+  if force or TODAY - modtime > datetime.timedelta(seconds=900):
+      import urllib.request
+      import urllib.error
+      with open(LEADERBOARD, 'wb') as f:
+          for year in YEARS:
+              url = f"https://adventofcode.com/{year}/leaderboard/private/view/173774.json"
+              req = urllib.request.Request(url)
+              req.add_header("Cookie", f"session={TOKEN}")
+              try:
+                  logging.info(f"Requesting leaderboard from {url}...")
+                  with urllib.request.urlopen(req) as r:
+                      f.write(r.read())
+                      f.write(b'\n')
+              except urllib.error.HTTPError as e:
+                  status_code = e.getcode()
+                  if status_code == 400:
+                      logging.error("Auth failed!")
+                      sys.exit(1)
+                  elif status_code == 404:
+                      logging.error("Day is not out yet???")
+                      sys.exit(1)
+                  else:
+                      logging.error(f"Request failed with code: {code}??")
+                      sys.exit(1)
+  else:
+      logging.info("Not enough time as elapsed to justify a new call.")
+  with open(LEADERBOARD) as f:
+      data = [json.loads(line) for line in f]
+      return data
+
+Event = namedtuple('Event', 'name time year day star')
+
+def recent_events(data):
+    """Print a summary of recent events."""
+    events = []
+    for yeardata in data:
+        year = yeardata['event']
+        for member in yeardata['members'].values():
+            name = member['name']
+            for day, level in member['completion_day_level'].items():
+                for star, data in level.items():
+                    events.append(Event(
+                        name=name,
+                        time=datetime.datetime.fromtimestamp(float(data['get_star_ts'])).astimezone(EAST_COAST),
+                        year=int(year),
+                        day=int(day),
+                        star=int(star)))
+    return sorted(events, key=lambda x: x.time, reverse=True)
+
+
+def total_leaderboard(events):
+    events = sorted(events, key=lambda x: x.name)
+    byname = itertools.groupby(events, key=lambda x: x.name)
+    totals = {name: sum(1 for x in events) for name, events in byname}
+    return sorted(totals.items(), key=lambda x: x[1], reverse=True)
+
+
 if __name__ == "__main__":
   logging.basicConfig(level=logging.INFO)
 
-  if len(sys.argv) > 2:
-    year = int(sys.argv[2])
-  else:
-    year = TODAY.year
+  data = get_leaderboard()
+  events = recent_events(data)
 
-  if len(sys.argv) > 1:
-    day = int(sys.argv[1])
-  else:
-    if TODAY.month != 12 and TODAY.day > 25:
-      logging.error("It isn't Advent!")
-      sys.exit(1)
-    day = TODAY.day
+  print("\n\nRECENT EVENTS\n========================")
+  for x in events[:15]:
+      print(f"{x.name} solved {x.year}-{x.day}-{x.star} {(TODAY-x.time).total_seconds()/(60*60):.2f} hours ago at {x.time}")
 
-  print(get_data(day, year))
+
+  leaderboard = total_leaderboard(events)
+
+  print("\n\nLEADERBOARD\n==========================")
+  for name, total in leaderboard:
+      print(f"{total:3d} - {name}")
+
 
