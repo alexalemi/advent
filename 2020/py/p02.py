44ef28deab4ab08f5407e48d4e719cd0cf020bf9
Alex Alemi <alexalemi@gmail.com>
2020-12-02 04:05:07 -0500

    day2

--- 2020/py/p02.py
+++ 2020/py/p02.py
@@ -1,19 +1,41 @@
+from collections import Counter
 from utils import data20
 
-data = data20(2)
+data = data20(2).strip()
 
-tests = []
+tests = [("""1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc""", 2)]
+
+
+def parse(line):
+  rule, password = line.strip().split(": ")
+  times, let = rule.split()
+  low, high = times.split("-")
+  low = int(low)
+  high = int(high)
+  return (low, high, let, password)
+
+
+def isvalid(line):
+  low, high, let, password = parse(line)
+  return low <= Counter(password)[let] <= high
 
 
 def answer1(inp):
-  return None
+  return sum(map(isvalid, inp.splitlines()))
 
 
-tests2 = []
+tests2 = [(tests[0][0], 1)]
+
+
+def isvalid2(line):
+  low, high, let, password = parse(line)
+  return (password[low - 1] == let) ^ (password[high - 1] == let)
 
 
 def answer2(inp):
-  return None
+  return sum(map(isvalid2, inp.splitlines()))
 
 
 if __name__ == "__main__":
--- 2020/racket/p01.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-
-(define raw-data (port->string (open-input-file "../input/01.txt") #:close? #t))
-(define data (map string->number (string-split raw-data "\n")))
-
-(displayln "Answer1")
-;; (print (answer1 data))
