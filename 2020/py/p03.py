42d27a5fce73c7f226d818dd485aac29a889e5c7
Alex Alemi <alexalemi@gmail.com>
2020-12-03 05:49:32 -0500

    day3

--- 2020/py/p03.py
+++ 2020/py/p03.py
@@ -1,20 +1,51 @@
 import time
 from utils import data20
+import math
 
 data = data20(3)
 
-tests = []
+tests = [("""..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#""", 7)]
 
 
-def answer1(inp):
-  return None
+def process(inp):
+  vals = {}
+  for y, line in enumerate(inp.splitlines()):
+    for x, c in enumerate(line):
+      vals[complex(x, y)] = c
+  return vals
 
 
-tests2 = []
+def answer1(inp, slope=(3, 1)):
+  vals = process(inp)
+  bottom = max(z.imag for z in vals)
+  right = max(z.real for z in vals)
+  start = complex(0, 0)
+  pos = start
+  total = 0
+  while pos.imag < bottom:
+    pos = pos + complex(*slope)
+    pos = complex(pos.real % (right + 1), pos.imag)
+    if vals[pos] == '#':
+      total += 1
+  return total
+
+
+tests2 = [(tests[0][0], 336)]
 
 
 def answer2(inp):
-  return None
+  return math.prod(
+      answer1(inp, slope) for slope in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)])
 
 
 if __name__ == "__main__":
