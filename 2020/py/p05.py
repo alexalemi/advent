c906a61b1c66655ad0dcc7be8d404544adf1e086
Alex Alemi <alexalemi@gmail.com>
2020-12-05 00:15:42 -0500

    update 5

--- 2020/py/p05.py
+++ 2020/py/p05.py
@@ -26,9 +26,6 @@ def answer1(inp):
   return max((make(line) for line in inp.splitlines()), key=lambda x: x.id).id
 
 
-tests2 = []
-
-
 def answer2(inp):
   seats = [make(line) for line in inp.splitlines()]
   ids = set(x.id for x in seats)
@@ -46,10 +43,6 @@ if __name__ == "__main__":
   ans1 = answer1(data)
   end = time.time()
   print("Answer1:", ans1, f" in {end - start:0.3e} secs")
-
-  for inp, ans in tests2:
-    myans = answer2(inp)
-    assert myans == ans, f"Failed on {inp} == {ans}, got {myans}!"
 
   start = time.time()
   ans2 = answer2(data)
