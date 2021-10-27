5cc3f09ac9620f14ec9074ea8138e8de3437d6ed
Alex Alemi <alexalemi@gmail.com>
2021-10-27 00:42:32 -0400

    more readable

--- 2017/clojure/p01.clj
+++ 2017/clojure/p01.clj
@@ -1,9 +1,9 @@
 (ns advent01
   (:require
-   [clojure.test :refer [deftest is run-tests]]  
-   [clojure.string :refer [trim]]))
+   [clojure.test :as test]
+   [clojure.string :as string]))
 
-(def data (trim (slurp "../input/01.txt")))
+(def data (string/trim (slurp "../input/01.txt")))
 
 (defn part-1 [data] 
   (->> data
@@ -19,11 +19,11 @@
         
 (def ans1 (part-1 data))
 
-(deftest test-part-1
-  (is (= (part-1 "1122") 3))
-  (is (= (part-1 "1111") 4))
-  (is (= (part-1 "1234") 0))
-  (is (= (part-1 "91212129") 9)))
+(test/deftest test-part-1
+  (test/is (= (part-1 "1122") 3))
+  (test/is (= (part-1 "1111") 4))
+  (test/is (= (part-1 "1234") 0))
+  (test/is (= (part-1 "91212129") 9)))
 
 (defn part-2 [data] 
   (let [n (count data)]
@@ -41,14 +41,14 @@
 
 (def ans2 (part-2 data))
 
-(deftest test-part-1
-  (is (= (part-2 "1212") 6))
-  (is (= (part-2 "1221") 0))
-  (is (= (part-2 "123425") 4))
-  (is (= (part-2 "123123") 12))
-  (is (= (part-2 "12131415") 4)))
+(test/deftest test-part-1
+  (test/is (= (part-2 "1212") 6))
+  (test/is (= (part-2 "1221") 0))
+  (test/is (= (part-2 "123425") 4))
+  (test/is (= (part-2 "123123") 12))
+  (test/is (= (part-2 "12131415") 4)))
 
-(run-tests)
+(test/run-tests)
 
 (println)
 (println "Part1:" ans1)
