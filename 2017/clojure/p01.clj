79ea4a279e02f32e51ed67cdea9b43648e3c6611
Alex Alemi <alexalemi@gmail.com>
2021-10-27 00:19:25 -0400

    with p1 done.

--- 2017/clojure/p01.clj
+++ 2017/clojure/p01.clj
@@ -3,11 +3,10 @@
    [clojure.test :refer [deftest is run-tests]]  
    [clojure.string :refer [trim]]))
 
-(def data (slurp "../input/01.txt"))
+(def data (trim (slurp "../input/01.txt")))
 
 (defn part-1 [data] 
   (->> data
-       (trim)
        (seq)
        (cons (last data))
        (partition 2 1)
@@ -19,8 +18,6 @@
        (reduce +)))
         
 (def ans1 (part-1 data))
-(println "Part1: " ans1)
-
 
 (deftest test-part-1
   (is (= (part-1 "1122") 3))
@@ -28,6 +25,31 @@
   (is (= (part-1 "1234") 0))
   (is (= (part-1 "91212129") 9)))
 
-;(run-tests)
+(defn part-2 [data] 
+  (let [n (count data)]
+    (->> data
+         (seq)
+         (split-at (/ n 2))
+         (apply interleave)
+         (partition 2)
+         (map 
+          (fn [pair] 
+            (if (= (first pair) (second pair))
+                (* 2 (Character/digit (first pair) 10))
+                0)))
+         (reduce +))))
 
+(def ans2 (part-2 data))
 
+(deftest test-part-1
+  (is (= (part-2 "1212") 6))
+  (is (= (part-2 "1221") 0))
+  (is (= (part-2 "123425") 4))
+  (is (= (part-2 "123123") 12))
+  (is (= (part-2 "12131415") 4)))
+
+(run-tests)
+
+(println)
+(println "Part1:" ans1)
+(println "Part2:" ans2)
