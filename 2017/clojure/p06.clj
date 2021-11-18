98889ab6366f60ff051f5005748604f871b2fb7f
Alex Alemi <alexalemi@gmail.com>
2021-11-17 21:03:30 -0500

    modifications to p06 2017 clojure

--- 2017/clojure/p06.clj
+++ 2017/clojure/p06.clj
@@ -1,12 +1,11 @@
 (ns advent06
   (:require
    [clojure.edn :as edn]
-   [clojure.test :as test]
-   [clojure.string :as string]))
-
+   [clojure.string :as string]
+   [clojure.test :as test]))
 
 (def data (into [] (map edn/read-string (string/split (slurp "../input/06.txt") #"\t"))))
-;; => (5 1 10 0 1 7 13 14 3 12 8 10 7 12 0 6)
+;; => [5 1 10 0 1 7 13 14 3 12 8 10 7 12 0 6]
 
 (defn maximum
   "Return the index of the maximum element"
@@ -18,72 +17,58 @@
 
   Takes the elements in the biggest bin and starts
   dealing them out to the other slots."
-  ([banks] 
+  ([banks]
    (let [[index blocks] (maximum banks)
-          new-banks (assoc banks index 0)
-          safe-index (mod (inc index) (count banks))]
+         new-banks (assoc banks index 0)
+         safe-index (mod (inc index) (count banks))]
      (redistribute new-banks safe-index blocks)))
-  ([banks index blocks] 
-   (if (> blocks 0) 
+  ([banks index blocks]
+   (if (> blocks 0)
      ; redistribute
      (recur (assoc banks index (inc (get banks index))) (mod (inc index) (count banks)) (dec blocks))
      banks)))
 
-
 (defn first-repeat
   "Figure out how many redistributions until a repeat"
   ([banks] (first-repeat banks #{} 0))
-  ([banks seen counter] 
+  ([banks seen counter]
    (let [new-banks (redistribute banks)]
      (if (contains? seen new-banks) (inc counter)
-       (recur new-banks (conj seen new-banks) (inc counter))))))
+         (recur new-banks (conj seen new-banks) (inc counter))))))
 
 (test/deftest test-part-1
-  (test/is (= (redistribute [0 2 7 0]) [2 4 1 2])) 
-  (test/is (= (redistribute [2 4 1 2]) [3 1 2 3])) 
-  (test/is (= (redistribute [3 1 2 3]) [0 2 3 4])) 
-  (test/is (= (redistribute [0 2 3 4]) [1 3 4 1])) 
-  (test/is (= (redistribute [1 3 4 1]) [2 4 1 2])) 
+  (test/are [x y] (= (redistribute x) y)
+    [0 2 7 0] [2 4 1 2]
+    [2 4 1 2] [3 1 2 3]
+    [3 1 2 3] [0 2 3 4]
+    [0 2 3 4] [1 3 4 1]
+    [1 3 4 1] [2 4 1 2]))
+
+(test/deftest test-part-1-2
   (test/is (= (first-repeat [0 2 7 0]) 5)))
 
 (time (def ans1 (first-repeat data)))
 
-(comment 
- (defn exit-2
-   "Find the exit of a sequence of instructions."
-   ([tape] (exit-2 0 0 (transient tape)))
-   ([loc step tape]
-    (if-let [val (get tape loc)]
-      (let [new-val ^int (if (>= val 3) (dec val) (inc val))]
-        (recur (+ loc val) (inc step) (assoc! tape loc new-val)))
-      step)))
+(comment "for part two we are supposed to figure out
+the length of the cycle.")
 
- (defn exit-2-array
-   "Find the exit of a sequence of instructions."
-   [tape]
-   (let [size (count tape)]
-     (loop [tape (int-array tape)
-            loc  0
-            step 0]
-       (if (or (< loc 0) (>= loc size)) step
-           (let [val ^int (aget tape loc)
-                 new-val ^int (if (>= val 3) (dec val) (inc val))]
-             (aset ^ints tape loc new-val)
-             (recur
-               ^ints tape
-               (+ loc val)
-               (inc step)))))))
+(defn cycle-length
+  "Figure out how many redistributions until a repeat"
+  ([banks] (cycle-length banks {} 0))
+  ([banks seen counter]
+   (let [new-banks (redistribute banks)]
+     (if (contains? seen new-banks) (- counter (get seen new-banks))
+         (recur new-banks (assoc seen new-banks counter) (inc counter))))))
 
- (test/deftest test-part-2
-   (test/is (= (exit-2-array [0 3 0 1 -3]) 10))) 
+(test/deftest test-part-2
+  (test/is (= (cycle-length [0 2 7 0]) 4)))
 
- (time (def ans2 (exit-2-array data))))
-
+(time (def ans2 (cycle-length data)))
 
 (test/run-tests)
 
 (println)
 (println "Answer1:", ans1)
-; (println "Answer2:", ans2)
+(println "Answer2:", ans2)
 
 
