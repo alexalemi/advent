30215929f4074f09fd2ee7dbf61c096cccd8354e
Alex Alemi <alexalemi@gmail.com>
2021-12-15 09:58:34 -0500

    2021-15-clj cleanup

--- 2021/clojure/p15.clj
+++ 2021/clojure/p15.clj
@@ -19,7 +19,11 @@
 (defn enumerate [coll]
   (zipmap (range) coll))
 
-(defn process [s]
+(defn process
+  "Read in the board string and output a collection:
+       {:costs :: loc -> cost
+        :goal :start}"
+  [s]
   (let [costs (into {} (for [[row line] (enumerate (str/split-lines s))
                              [col s] (enumerate line)]
                          [[col row] (read-string (str s))]))
@@ -30,22 +34,28 @@
 (def data (process data-string))
 (def test-data (process test-string))
 
-(defn abs [x] (max x (- x)))
-
-(defn heuristic [end pos]
-  (let [[end-x end-y] end
-        [pos-x pos-y] pos]
-    (+ (abs (- end-x pos-x))
-       (abs (- end-y pos-y)))))
-
-(defn neighbors [loc]
+(defn neighbors
+  "Return up down left and right of a loc."
+  [loc]
   (let [[x y] loc]
     [[(inc x) y]
      [(dec x) y]
      [x (inc y)]
      [x (dec y)]]))
 
-(defn reconstruct-path [came-from current]
+;; First implementation general A-star algorithm
+
+(defn abs [x] (max x (- x)))
+
+(defn manhattan [end pos]
+  (let [[end-x end-y] end
+        [pos-x pos-y] pos]
+    (+ (abs (- end-x pos-x))
+       (abs (- end-y pos-y)))))
+
+(defn reconstruct-path
+  "Knowing the parent of each node, construct the full path."
+  [came-from current]
   (loop [current current
          path (list current)]
     (let [prev (came-from current)]
@@ -53,7 +63,9 @@
         (recur prev (conj path prev))
         path))))
 
-(defn a-star [costs goal start h]
+(defn a-star
+  "General A-star algorithm."
+  [costs goal start h]
   (loop [frontier (priority-map start (h start))
          came-from {}
          best-score {start 0}
@@ -91,28 +103,69 @@
              prev
              (rest neighs))))))))
 
-(defn shortest-cost [data]
+(defn shortest-cost-old [data]
   (let [{:keys [costs goal start]} data
-        h (partial heuristic goal)]
+        h (partial manhattan goal)]
     (reduce + (map costs (rest (a-star costs goal start h))))))
+
+;; Second implementation: simple dijkstra
+
+(defn neighbor-costs
+  "Returns a map of all neighbors to their travel costs."
+  [costs loc]
+  (let [ns (filter costs (neighbors loc))
+        cs (map costs ns)]
+   (zipmap ns cs)))
+
+(defn map-val
+  "Map a function over the vals of a map."
+  [m f & args]
+  (reduce-kv (fn [m k v] (assoc m k (apply f v args))) {} m))
+
+(defn remove-keys
+  "Remove some of the keys in a map."
+  [pred m]
+  (reduce-kv (fn [m k v] (if (pred k) m (assoc m k v))) {} m))
+
+(defn shortest-cost
+  "Recursive dijstra's algorithm."
+  [data]
+  (let [{:keys [costs start goal]} data
+        f (partial neighbor-costs costs)]
+    (loop [frontier (priority-map start 0)  ; the nodes we have to expand
+           min-cost {start 0}]  ; the nodes we already know the optimal cost of.
+     (let [[current cost] (peek frontier)
+           ; for all of the neighbors of the current node, see if we've just
+           ; discovered a shorter path.
+           dists (map-val (remove-keys min-cost (f current)) + cost)]
+       (if (= current goal) cost
+         (recur (merge-with min (pop frontier) dists)
+                (assoc min-cost current cost)))))))
 
 (test/deftest test-part-1
   (test/is (= (shortest-cost test-data) 40))
   (test/is (= (shortest-cost data) 714)))
 
+(time (def ans1 (shortest-cost-old data)))
 (time (def ans1 (shortest-cost data)))
 (println)
 (println "Answer 1:" ans1)
 
-(defn vec-add [a b] (mapv + a b))
+(defn vec-add
+  "vector addition"
+  [a b] (mapv + a b))
 
-(defn wrap-add [x n]
+(defn wrap-add
+  "Adds n to x but 9 wraps around to 1."
+  [x n]
   (cond
     (= n 0) x
     (= x 9) (recur 1 (dec n))
     :else (recur (inc x) (dec n))))
 
-(defn expand-map [data]
+(defn expand-map 
+  "Expand the map 5x in both directions."
+  [data]
   (let [{:keys [costs goal]} data
         [width height] (map inc goal)]
     (-> data
