6a311b4c85bae2fc227d5366b70b2a1bdd2b5a2c
Alex Alemi <alexalemi@gmail.com>
2021-12-21 14:02:02 -0500

    2017-24-clj

--- 2017/clojure/p24.clj
+++ 2017/clojure/p24.clj
@@ -3,7 +3,6 @@
    [clojure.test :as test]
    [clojure.string :as str]))
 
-
 (def data-string (slurp "../input/24.txt"))
 (def test-string "0/2
 2/2
@@ -25,71 +24,67 @@
 (def data (process data-string))
 (def test-data (process test-string))
 
-(defn subtract [s vals]
-  (if (empty? vals) s
-      (recur (disj s (first vals)) (rest vals))))
-
-(defn score [path]
-  (reduce + (flatten path)))
-
 (defn next-link [end]
   (fn [x]
     (let [[a b] x]
-     (cond
-       (= a end) [x b]
-       (= b end) [x a]
-       :else nil))))
-
-(defn longest-ends [data]
-  (loop [partials [{:end 0 :path ()}]
-         longest {0 0}]
-    (let [front (first partials)
-          {:keys [end path]} front
-          best-so-far (get longest end 0)
-          score-so-far (score path)
-          available (subtract data path)
-          links (filter
-                 (fn [[piece end]] (> (+ score-so-far (apply + piece)) (get longest end 0)))
-                 (filter some? (map (next-link end) available)))]
-      ; (println "longest = " longest "num partials= " (count partials))
       (cond
-        (empty? partials)
-        longest
+        (= a end) [x b]
+        (= b end) [x a]
+        :else nil))))
 
-        (empty? links)
-        (recur (rest partials)
-               (assoc longest
-                      end
-                      (max best-so-far (score path))))
-
-        :else
-        (recur
-         (concat (rest partials)
-                 (for [[next end] links]
-                   {:end end :path (conj path next)}))
-         longest)))))
-
-(defn max-depth [data]
-  (fn inner-max-depth [front]
-    (let [{:keys [end path]} front
-          score-so-far (score path)
-          available (subtract data path)
-          links (filter some? (map (next-link end) available))]
-      (if
-        (empty? links) score-so-far
-        (reduce max (for [[next end] links]
-                         (inner-max-depth {:end end :path (conj path next)})))))))
+(defn max-score [data]
+  (let [f (fn inner-max-score [inner front]
+            (let [inner (fn [a] (inner inner a))
+                  {:keys [end available]} front
+                  links (filter some? (map (next-link end) available))]
+              (if
+               (empty? links) 0
+               (reduce max (for [[next end] links]
+                             (apply + (inner {:end end :available (disj available next)}) next))))))
+        memo-f (memoize f)
+        memo-f (partial memo-f memo-f)]
+    (memo-f {:end 0 :available data})))
 
 (defn part-1 [data]
-  (reduce max (vals (longest-ends data))))
-
-(part-1 test-data)
+  (max-score data))
 
 (test/deftest test-part-1
   (test/is (= (part-1 test-data) 31)))
 
-; (time (def ans1 (part-1 data)))
-; (println)
-; (println "Answer1:" ans1)
+(time (def ans1 (max-score data)))
+(println)
+(println "Answer1:" ans1)
+
+(defn path-max [[l1 s1] [l2 s2]]
+  (cond
+    (> l1 l2) [l1 s1]
+    (< l1 l2) [l2 s2]
+    (= l1 l2)
+    (if (> s2 s1) [l1 s2] [l1 s1])))
+
+(defn max-length [data]
+  (let [f (fn inner-max-length [inner front]
+            (let [inner (fn [a] (inner inner a))
+                  {:keys [end available]} front
+                  links (filter some? (map (next-link end) available))]
+              (if
+               (empty? links) [0 0]
+               (reduce path-max (for [[next end] links]
+                                  (mapv +
+                                        (inner {:end end :available (disj available next)})
+                                        [1 (reduce + next)]))))))
+        memo-f (memoize f)
+        memo-f (partial memo-f memo-f)]
+    (memo-f {:end 0 :available data})))
+
+(defn part-2 [data]
+  (second (max-length data)))
+
+(test/deftest test-part-2
+  (test/is (= (part-2 test-data) 19)))
+
+(time (def ans2 (part-2 data)))
+(println)
+(println "Answer 2:" ans2)
 
 (test/run-tests)
