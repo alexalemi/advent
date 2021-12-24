b847a3d1974bb79827861b8153695196859b2436
Alex Alemi <alexalemi@gmail.com>
2021-12-24 15:33:23 -0500

    new matcher

--- 2021/clojure/p24.clj
+++ 2021/clojure/p24.clj
@@ -60,155 +60,137 @@ mod w 2"])
 (defn mul [state reg b]
   (let [a (state reg)
         b (value state b)]
-    (cond
-      (-zero? b)
-      (assoc state reg 0)
-
-      (-zero? a)
-      (assoc state reg 0)
-
-      (one? b)
-      (assoc state reg a)
-
-      (one? a)
-      (assoc state reg b)
-
-      (and (number? a) (number? b))
-      (assoc state reg (* a b))
-
-      (and (number? b) ; (* 26 (+ sym num)) -> (+ (* 26 sym) (* 26 num))
-           (seq? a)
-           (= (first a) :+)
-           (keyword? (second a))
-           (number? (third a)))
-      (assoc state reg `(:+ (:* ~b ~(second a)) ~(* b (third a))))
-
-      :else
-      (assoc state reg `(:* ~a ~b)))))
+     (assoc state reg (list :* a b))))
 
 (defn add [state reg b]
   (let [a (state reg)
         b (value state b)]
-    (cond
-      (-zero? b)
-      (assoc state reg a)
-
-      (-zero? a)
-      (assoc state reg b)
-
-      (and (number? a) (number? b))
-      (assoc state reg (+ a b))
-
-      (and (number? b); (+ (+ :let num) num)
-           (seq? a)
-           (= (first a) :+)
-           (keyword? (second a))
-           (number? (third a)))
-      (assoc state reg (:+ (second a) (+ b (third a))))
-
-      (and (seq? a) ; (+ (+ X num) (+ Y num))
-           (= (first a) :+)
-           (number? (third a))
-           (seq? b)
-           (= (first b) :+)
-           (number? (third b)))
-      (assoc state reg `(:+ ~(second a) ~(second b) ~(+ (third b) (third a))))
-
-      :else
-      (assoc state reg `(:+ ~a ~b)))))
+    (assoc state reg (list :+ a b))))
 
 (defn -mod [state reg b]
   (let [a (state reg)
         b (value state b)]
-    (cond
-      (-zero? a)
-      (assoc state reg 0)
-
-      (and (number? a) (number? b))
-      (assoc state reg (mod a b))
-
-      (and (number? b)
-           (= (first a) :+)  ; (mod (+ :let num) 26)
-           (keyword? (second a))
-           (number? (third a))
-           (> (- b (third a)) 9))
-      (assoc state reg a)
-
-      :else
-      (assoc state reg `(:mod ~a ~b)))))
+    (assoc state reg (list :mod a b))))
 
 
 (defn div [state reg b]
   (let [a (state reg)
         b (value state b)]
-    (cond
-      (-zero? a)
-      (assoc state reg 0)
-
-      (one? b)
-      (assoc state reg a)
-
-      (and (number? a) (number? b))
-      (assoc state reg (quot a b))
-
-      (and (number? b) ; (div (+ (* 26 X) (+ :let num)) 26) -> X
-           (= b 26)
-           (= (first a) :+)
-           (= (first (second a)) :*) ; special case
-           (= (second (second a)) 26)
-           (= (first (third a)) :+)
-           (keyword? (second (third a)))
-           (< (third (third a)) (- 26 9)))
-      (assoc state reg (third (second a)))
-
-      :else
-      (assoc state reg `(:div ~a ~b)))))
+    (assoc state reg (list :div a b))))
 
 (defn eql [state reg b]
   (let [a (state reg)
         b (value state b)]
-    (cond
-      (= a b)
-      (assoc state reg 1)
+    (assoc state reg (list := a b))))
 
-      (and (number? a) (number? b) (not= a b))
-      (assoc state reg 0)
+(def rules
+  {'(:* (? x) 0) 0
+   '(:* 0 (? x)) 0
+   '(:* (? x) 1) 'x
+   '(:* 1 (? x)) 'x
+   '(:* (?c A) (?c B)) '(* A B)})
 
-      (and (number? a) (keyword? b) (> a 9)) ; symbols can't be more than 9
-      (assoc state reg 0)
+(defn wildcard? [x]
+  (and (list? x)
+       (= (first x) '?)))
 
-      :else
-      (assoc state reg `(:= ~a ~b)))))
+(defn wildcard-variable? [x]
+  (and (list? x)
+       (= (first x) '?v)))
 
+(defn wildcard-constant? [x]
+  (and (list? x)
+       (= (first x) '?c)))
+
+(defn wildcard-form [x] (second x))
+
+(defn atom? [x] (not (coll? x)))
+
+(defn wildcard-match [bindings var expr]
+  (let [prev (bindings var)]
+   (cond
+     (nil? prev) (assoc bindings var expr)
+     (= prev expr) bindings
+     :else nil)))
+
+(defn matcher
+  "Tries to match the rule against the expression,
+  if not, nil otherwise return a dictionary
+  of bindings."
+  ([rule expr] (matcher rule expr {}))
+  ([rule expr bindings]
+   (println "rule=" rule "expr=" expr "bindings=" bindings)
+   (cond
+     (nil? bindings) nil  ; short-circuit a failed match
+
+     (atom? rule)
+     (if (= rule expr) bindings nil)
+
+     (wildcard? rule)
+     (wildcard-match bindings (wildcard-form rule) expr)
+
+     (wildcard-constant? rule)
+     (if (number? expr)
+       (wildcard-match bindings (wildcard-form rule) expr)
+       nil)
+
+     (wildcard-variable? rule)
+     (if (keyword? expr)
+       (wildcard-match bindings (wildcard-form rule) expr)
+       nil)
+
+     (empty? rule)
+     (if (empty? expr) bindings nil)
+
+     (empty? expr)
+     (if (empty? rule) bindings nil)
+
+     (atom? expr) nil
+
+     :else
+     (matcher (rest rule)
+              (rest expr)
+              (matcher
+               (first rule)
+               (first expr)
+               bindings)))))
+
+(defn evaluator
+  "Instantiate the skeleton with the binding."
+  [binding skeleton])
+
+
+(defn simplify [expr]
+  expr)
+
+(defn simplify-all [state]
+  (-> state
+      (update :x simplify)
+      (update :y simplify)
+      (update :z simplify)
+      (update :w simplify)))
 
 (defn step [state inst]
   (let [[op a b] inst]
-    (case op
+    (simplify
+     (case op
       :inp (inp state a)
       :mul (mul state a b)
       :add (add state a b)
       :mod (-mod state a b)
       :div (div state a b)
-      :eql (eql state a b))))
-
-(let [state state
-      prog data
-      n 44
-      prog (take n prog)]
-  (println n (last prog))
-  (reduce step state prog))
-;; => (:+ (:* 26 (:+ (:* 26 (:+ (:* 26 (:+ (:* 26 (:+ (:* 26 (:+ (:* 26 (:+ :a 16)) (:+ :b 11))) (:+ :e 12))) (:+ :f 2))) (:+ :h 4))) (:+ :i 12))) (:+ :n 15))
+      :eql (eql state a b)))))
 
 
 
+(comment
+  (defn base-26 [vals]
+   (loop [vals vals
+          num 0]
+     (if (empty? vals) num
+         (recur (rest vals) (+ (* num 26) (first vals))))))
 
-(defn base-26 [vals]
-  (loop [vals vals
-         num 0]
-    (if (empty? vals) num
-        (recur (rest vals) (+ (* num 26) (first vals))))))
-
-(base-26 [16 11 12 2 4 12 15])
+  (base-26 [16 11 12 2 4 12 15]))
 
 (comment
  (let [state (assoc state :inp (list 0 0 9 9 0 0 9 0 0 9 9 9 9 0))
@@ -219,3 +201,11 @@ mod w 2"])
   (let [[a b c d e f g h i j k l m n] [0 0 9 9 0 0 9 0 0 9 9 9 9 0]]
    (+ (* 26 (+ (* 26 (+ (* 26 (+ (* 26 (+ (* 26 (+ (* 26 (+ a 16)) (+ b 11))) (+ e 12))) (+ f 2))) (+ h 4))) (+ i 12))) (+ n 15))))
 
+
+(let [state state
+      prog data
+      n 5 ; 44
+      prog (take n prog)]
+  (println n (last prog))
+  (reduce step state prog))
+;; => (:+ (:* 26 (:+ (:* 26 (:+ (:* 26 (:+ (:* 26 (:+ (:* 26 (:+ (:* 26 (:+ :a 16)) (:+ :b 11))) (:+ :e 12))) (:+ :f 2))) (:+ :h 4))) (:+ :i 12))) (:+ :n 15))
