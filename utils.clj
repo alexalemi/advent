8e6618ccb166992f04b5cab63272657e02d27c00
Alex Alemi <alexalemi@gmail.com>
2022-11-22 14:42:09 -0500

    dot tokens

--- utils.clj
+++ utils.clj
@@ -6,6 +6,6 @@
 ; (def ROOT "https://adventofcode.com/{year}/leaderboard/private/view/{num}.json")
 (def ROOT "https://adventofcode.com/2021/leaderboard/private/view/173774.json")
 
-(def TOKEN (str/trim (slurp "token.txt")))
+(def TOKEN (str/trim (slurp ".token")))
 
 (def resp (curl/get ROOT {:headers {"Cookie" (str "session=" TOKEN "")}}))
