766f820429d4a2e564688f99f52d4eb15ce9bf93
Alex Alemi <alexalemi@gmail.com>
2023-12-02 21:20:31 -0500

    with type hints

--- 2023/python/p02.py
+++ 2023/python/p02.py
@@ -1,6 +1,7 @@
 import parse
 import math
 from utils import data23
+from typing import Iterator, Dict
 
 data = data23(2)
 
@@ -10,22 +11,31 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
 
-def find_color(line, color):
-    return (match['num'] for match in parse.findall(f"{{num:d}} {color}", line))
 
-def summary(line):
+def find_color(line: str, color: str) -> Iterator[int]:
+    return (match["num"] for match in parse.findall(f"{{num:d}} {color}", line))
+
+
+Summary = Dict[str, int]
+
+
+def summary(line: str) -> Summary:
     return {color: max(find_color(line, color)) for color in ("red", "green", "blue")}
 
-def game_id(line):
-    return parse.search("Game {game_id:d}", line)['game_id']
 
-def is_valid(total):
-    return (total['red'] <= 12) and (total['green'] <= 13) and (total['blue'] <= 14)
+def game_id(line: str) -> int:
+    return parse.search("Game {game_id:d}", line)["game_id"]
 
-def part1(data):
+
+def is_valid(total: Summary) -> bool:
+    return (total["red"] <= 12) and (total["green"] <= 13) and (total["blue"] <= 14)
+
+
+def part1(data: str) -> int:
     summaries = {game_id(line): summary(line) for line in data.splitlines()}
-    valids = {k:is_valid(v) for k,v in summaries.items()}
-    return sum(k for k,v in valids.items() if v)
+    valids = {k: is_valid(v) for k, v in summaries.items()}
+    return sum(k for k, v in valids.items() if v)
+
 
 assert part1(test_data) == 8
 
@@ -33,13 +43,16 @@ ans1 = part1(data)
 
 assert ans1 == 2541
 
-def power(summary):
+
+def power(summary: Summary) -> int:
     return math.prod(summary.values())
 
-def part2(data):
+
+def part2(data: str) -> int:
     summaries = (summary(line) for line in data.splitlines())
     powers = (power(x) for x in summaries)
     return sum(powers)
+
 
 assert part2(test_data) == 2286
 
@@ -49,6 +62,6 @@ assert ans2 == 66016
 
 
 if __name__ == "__main__":
-  print("Answer1:", ans1)
+    print("Answer1:", ans1)
 
-  print("Answer2:", ans2)
+    print("Answer2:", ans2)
