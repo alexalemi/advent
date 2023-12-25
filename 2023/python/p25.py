6cb0f2f7fd8f765f847583cb6666370cbabe3a1d
Alex Alemi <alexalemi@gmail.com>
2023-12-25 07:59:44 -0500

    2025 complete!

---
+++ 2023/python/p25.py
@@ -0,0 +1,66 @@
+# Advent of Code 2023 - Day 25 - Snowverload
+
+import networkx as nx
+import math
+
+with open("../input/25.txt") as f:
+    data_string = f.read()
+
+test_string = """jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr"""
+
+def process(s: str) -> dict[str, set[str]]:
+    graph = {}
+    for line in s.splitlines():
+        node, children = line.split(": ")
+        for child in children.split():
+            graph.setdefault(node, set()).add(child)
+            graph.setdefault(child, set()).add(node)
+    return graph
+
+test_data = process(test_string)
+data = process(data_string)
+
+graph = nx.Graph(data)
+
+new_data = data.copy()
+to_remove = [('dgc', 'fqn'), ('vps', 'htp'), ('rpd', 'ttj')]
+
+for (fm, to) in to_remove:
+   new_data[fm].remove(to)
+   new_data[to].remove(fm)
+
+def find_clusters(graph):
+    g = graph.copy()
+    sizes = []
+    while g:
+        cluster = set()
+        frontier = [next(iter(g))]
+        while frontier:
+            new = frontier.pop()
+            cluster.add(new)
+            for neigh in g[new]:
+                if neigh in g and neigh not in cluster:
+                    frontier.append(neigh)
+        sizes.append(len(cluster))
+        for x in cluster:
+            del g[x]
+    return sizes
+
+ans1 = math.prod(find_clusters(new_data))
+assert ans1 == 600225
+
+if __name__ == "__main__":
+    print("Answer1:", ans1)
+
