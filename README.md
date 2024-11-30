fa8452ce58ca10bb7c52715f6baebbc427ead1f4
Alex Alemi <alexalemi@gmail.com>
2024-11-30 10:45:52 -0500

    Not ready for 2024 badges yet

--- README.md
+++ README.md
@@ -11,7 +11,6 @@ This repository contains my solutions in several languages to [Advent Of Code](h
 ![2021](./scripts/img/2021.svg)
 ![2022](./scripts/img/2022.svg)
 ![2023](./scripts/img/2023.svg)
-![2023](./scripts/img/2024.svg)
 
 ![Total](./scripts/img/Total.svg)
 
--- scripts/badges.clj
+++ scripts/badges.clj
@@ -21,7 +21,7 @@
 (def current-year (str (.getYear now)))
 (def current-day (str (.getDayOfMonth now)))
 
-(def yrs ["2022" "2021" "2020" "2019" "2018" "2017" "2016" "2015"])
+(def yrs ["2024" "2023" "2022" "2021" "2020" "2019" "2018" "2017" "2016" "2015"])
 
 (def USER-AGENT "github.com/alexalemi/advent by alexalemi@gmail.com")
 (def cookie (str/trim (slurp ".token")))
