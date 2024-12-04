2e872374df43df5e77f486d58dcb7bf65de77bc8
Alex Alemi <alexalemi@gmail.com>
2024-12-04 16:17:59 -0500

    2024-04-py

--- 2024/python/p04.py
+++ 2024/python/p04.py
@@ -1,6 +1,7 @@
 # Advent of Code 2024 - Day 4
 
 type Coord = tuple[int, int]
+type Board = dict[Board, chr]
 
 with open("../input/04.txt") as f:
     raw_data = f.read()
@@ -16,7 +17,7 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX"""
 
-def process(s: str) -> dict[Coord, chr]:
+def process(s: str) -> Board:
     return {(x,y): c for y, line in enumerate(s.splitlines())
                      for x, c in enumerate(line)}
 
@@ -59,7 +60,7 @@ def match(board, loc, direction, query='XMAS'):
         loc = direction(loc)
     return True
 
-def count_xmas(board: dict[tuple[int, int], chr]) -> int:
+def count_xmas(board: Board) -> int:
     count = 0
     for start in board:
         for direction in (north, south, east, west, northwest, northeast, southwest, southeast):
@@ -69,13 +70,55 @@ def count_xmas(board: dict[tuple[int, int], chr]) -> int:
 
 assert count_xmas(test_data) == 18, "Failed Part 1 test"
 ans1 = count_xmas(data)
+assert ans1 == 2591, "Failed part 1!"
 
 ## Part 2
 ## find the X-MASes
 
+def match_x_mas(board: Board, start: Coord) -> bool:
+    get = lambda fn: board.get(fn(start))
+    identity = lambda x: x
+
+    if get(identity) != 'A':
+        return False
+
+    if get(northwest) == 'M':
+        if get(southeast) != 'S':
+            return False
+    elif get(southeast) == 'M':
+        if get(northwest) != 'S':
+            return False
+    else:
+        return False
+
+    if get(northeast) == 'M':
+        if get(southwest) != 'S':
+            return False
+    elif get(southwest) == 'M':
+        if get(northeast) != 'S':
+            return False
+    else:
+        return False
+
+    return True
+
+
+def count_x_mas(board: Board) -> int:
+    count = 0
+    for start in board:
+        if match_x_mas(board, start):
+            count += 1
+    return count
+
+assert count_x_mas(test_data) == 9, "Failed part 2 test!"
+ans2 = count_x_mas(data)
+assert ans2 == 1880, "Failed part 2!"
+
+
 
 if __name__ == "__main__":
     print(f"Answer 1: {ans1}")
+    print(f"Answer 2: {ans2}")
 
 
 
