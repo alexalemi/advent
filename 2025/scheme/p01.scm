48556e1866fefddabe6cf78087d164868f2e9bf8
Alex Alemi <alexalemi@gmail.com>
2026-06-24 22:52:06 -0400

    2025-02-scm

--- 2025/scheme/p01.scm
+++ 2025/scheme/p01.scm
@@ -1,4 +1,4 @@
-
+;; # Advent of Code 2025 - Day 1
 
 (load "batteries.scm")
 (load "prelude.scm")
@@ -20,57 +20,58 @@ L82" "\n"))
 (let ((inst (test-data 0)))
   (string->number (substring inst 1)))
 
+(define (right x y) (modulo (+ x y) 100))
+(define (left x y) (modulo (- x y) 100))
 
 (define (dial insts)
-  (define (right x y) (modulo (+ x y) 100))
-  (define (left x y) (modulo (- x y) 100))
   (define (iter loc insts zeros)
     (if (empty? insts) zeros
-      (let* ((inst (first insts))
-             (dir (inst 0))
-             (amt (string->number (substring inst 1)))
-             (new-zeros (if (= loc 0) (inc zeros) zeros)))
-        (cond 
-          ((char=? #\L dir) (iter (left loc amt) (rest insts) new-zeros))
-          ((char=? #\R dir) (iter (right loc amt) (rest insts) new-zeros))
-          (else 'error)))))
+        (let* ((inst (first insts))
+               (dir (inst 0))
+               (amt (string->number (substring inst 1)))
+               (new-zeros (if (= loc 0) (inc zeros) zeros)))
+          (cond 
+            ((char=? #\L dir) (iter (left loc amt) (rest insts) new-zeros))
+            ((char=? #\R dir) (iter (right loc amt) (rest insts) new-zeros))
+            (else 'error)))))
   (iter 50 insts 0))
 
 (test (dial test-data) 3)
 
 (define ans-1 (dial raw-data))
-(display "Answer 1: ") 
-(display ans-1)
-(newline)
+(begin
+  (display "Answer 1: ") 
+  (display ans-1)
+  (newline))
 
 
 ;; Part 2
 
-(define (new-zeros loc op amt)
-  (define (iter zeros loc amt)
-    (if (zero? amt) (if (zero? loc) (inc zeros) zeros)
-     (iter (if (zero? loc) (inc zeros) zeros)
-           (modulo (op loc 1) 100)
-           (dec amt))))
-  (iter 0 (modulo (op loc 1) 100) (dec amt)))
 
 (define (new-dial insts)
-  (define (right x y) (modulo (+ x y) 100))
-  (define (left x y) (modulo (- x y) 100))
+  (define (count-zeros loc op amt)
+    (define (iter zeros loc amt)
+      (if (zero? amt) (if (zero? loc) (inc zeros) zeros)
+          (iter (if (zero? loc) (inc zeros) zeros)
+                (modulo (op loc 1) 100)
+                (dec amt))))
+    (iter 0 (modulo (op loc 1) 100) (dec amt)))
   (define (iter loc insts zeros)
     (if (empty? insts) zeros
-      (let* ((inst (first insts))
-             (dir (inst 0))
-             (amt (string->number (substring inst 1))))
-        (cond 
-          ((char=? #\L dir) (iter (left loc amt) (rest insts) (+ zeros (new-zeros loc - amt))))
-          ((char=? #\R dir) (iter (right loc amt) (rest insts) (+ zeros (new-zeros loc + amt))))
-          (else 'error)))))
+        (let* ((inst (first insts))
+               (dir (inst 0))
+               (amt (string->number (substring inst 1))))
+          (cond 
+            ((char=? #\L dir) (iter (left loc amt) (rest insts) (+ zeros (count-zeros loc - amt))))
+            ((char=? #\R dir) (iter (right loc amt) (rest insts) (+ zeros (count-zeros loc + amt))))
+            (else 'error)))))
   (iter 50 insts 0))
 
 (test (new-dial test-data) 6)
 
 (define ans-2 (new-dial raw-data))
-(display "Answer 2: ") 
-(display ans-2)
-(newline)
+
+(begin
+  (display "Answer 2: ") 
+  (display ans-2)
+  (newline))
---
+++ 2025/scheme/p02.scm
@@ -0,0 +1,75 @@
+;; # Advent of Code 2025 - Day 2
+
+(load "batteries.scm")
+(load "judge.scm")
+
+(define data-string (string-trim (slurp "../input/02.txt")))
+(define test-string "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124")
+
+(define (formatter str) 
+  (map (lambda (s) (map string->number (string-split s "-"))) 
+       (string-split str ",")))
+
+(define data (formatter data-string))
+(define test-data (formatter test-string))
+
+(define (invalid? num)
+  (let* ((s (number->string num))
+         (n (length s)))
+    (and (even? n) 
+         (string=? (substring s 0 (/ n 2))
+                   (substring s (/ n 2))))))
+
+
+(define (inc x) (+ x 1))
+(define (dec x) (- x 1))
+
+(define (invalid-ids segment)
+  (let ((lo (car segment))
+        (hi (cadr segment)))
+    (define (iter cur ids)
+      (if (> cur hi) ids
+          (iter (inc cur)
+                (if (invalid? cur) (cons cur ids) ids))))
+    (iter lo '())))
+
+(define (invalid-id-total data)
+  (apply + (map (lambda (segment) (apply + (invalid-ids segment)))
+                data)))
+
+
+(test (invalid-id-total test-data) 1227775554)
+
+(define ans-1 (invalid-id-total data))
+
+(begin
+  (display "Answer 1: ")
+  (display ans-1)
+  (newline))
+
+
+;; ## Part 2
+
+;; We have to redefine our notion of invalid to include any repeated substring.
+
+(define (repeated? s x)
+  (string=? s (string-repeat (substring s 0 x) (/ (length s) x))))
+
+(define (invalid? num)
+  (let* ((s (number->string num))
+         (n (length s)))
+    (define (iter x)
+      (if (zero? x) #f
+          (or (and (zero? (modulo n x))
+                   (repeated? s x))
+              (iter (dec x)))))
+    (iter (dec n))))
+
+(test (invalid-id-total test-data) 4174379265)
+
+(define ans-2 (invalid-id-total data))
+
+(begin
+  (display "Answer 2: ")
+  (display ans-2)
+  (newline))
